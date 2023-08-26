80599b1b0eddd599c0a964ee2bed20248fc9a920
thisismalindu <thisismalindu@gmail.com>
2023-08-26 08:35:13 +0530

    created my first express server with routes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
-
 const app = express();
 
 app.get("/", function (req,res) {
-    res.send("hello");
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/contact", function (req, res) {
+    res.send("Contact Mee");
 })
 
 app.listen(3000, function () {
     console.log("Server running on port 3000.");
 });
+
