48f1a5737429b66ef1f4de51a9540bf6ca76fc0b
thisismalindu <thisismalindu@gmail.com>
2023-08-26 16:46:44 +0530

    renamed app.html to index.html

--- calculator-with-backend/index.js
+++ calculator-with-backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/",function(req, res){
-    res.sendFile(__dirname + "/app.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res){
